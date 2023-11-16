3914980718ef674a8b1bcb7ea146c16532a5d040
rafikulislam775 <islamrafikul.jp@gmail.com>
2023-11-16 09:13:13 +0900

    post the user card data and get data by user email

---
+++ index.js
@@ -0,0 +1,85 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+require("dotenv").config();
+const port = process.env.PORT || 5001;
+//middleware
+app.use(cors());
+app.use(express.json());
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.hfblnyz.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Get the database and collection on which to run the operation
+    const menuCollection = client.db("BossDB").collection("menu");
+    const reviewsCollection = client.db("BossDB").collection("reviews");
+    const cartCollection = client.db("BossDB").collection("carts");
+    //get all menus items
+    app.get("/menu", async (req, res) => {
+      const result = await menuCollection.find().toArray();
+      res.send(result);
+    });
+    //get all reviews
+    app.get("/review", async (req, res) => {
+      const result = await reviewsCollection.find().toArray();
+      res.send(result);
+    });
+    //carts collection
+    app.post("/carts", async (req, res) => {
+      const cartItem = req.body;
+      const result = await cartCollection.insertOne(cartItem);
+      res.send(result);
+    });
+    //post means add kora data k load korb get diye
+    // email base kojar jnn
+    app.get("/carts", async (req, res) => {
+      const email = req.query.email;
+      const query = {email:email}
+      const result = await cartCollection.find(query).toArray();
+      res.send(result);
+    });
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("server is running");
+});
+app.listen(port, () => {
+  console.log(`server running on port ${port}`);
+});
+/**
+-------------------------
+      naming convention
+----------------------------
+
+app.get('/users')
+app.get('/users'/:id)
+app.post('/users')
+app.post('/users'/:id)
+app.put('/users')
+app.patch('/users'/:id)
+app.delete('/users'/:id)
+
+**/
